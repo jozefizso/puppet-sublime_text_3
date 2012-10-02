c90dfeecbccba974e7b6a02320be4a776e36ebf1
Will Farrington <wfarr@github.com>
2012-10-02 15:47:38 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Puppet sublime_text_2 for Boxen
+
+FIXME: update me bro
+
---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+class sublime_text_2 {
+  package { 'SublimeText2':
+    provider => 'appdmg',
+    source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg';
+  }
+}
