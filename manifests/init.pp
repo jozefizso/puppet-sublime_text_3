2dd5efc0915249ba4d5b5824ff077b8327d4010d
Chris Boyle <chris@bbck.net>
2013-03-05 16:35:59 -0500

    Just do it by default.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,16 @@
 # Usage:
 #
 #     include sublime_text_2
-class sublime_text_2 ( $enable_cli = true ) {
-  if $enable_cli {
-    file { "${boxen::config::bindir}/subl":
-      ensure  => link,
-      target  => '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl',
-      mode    => '0755',
-      require => Package['SublimeText2'],
-    }
-  }
-
+class sublime_text_2 {
   package { 'SublimeText2':
     provider => 'appdmg',
     source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg';
+  }
+
+  file { "${boxen::config::bindir}/subl":
+    ensure  => link,
+    target  => '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl',
+    mode    => '0755',
+    require => Package['SublimeText2'],
   }
 }
