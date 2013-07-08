28e50748eb924bd92857e8bc1570a9fcd8a3272d
Salimane Adjao Moustapha <me@salimane.com>
2013-07-08 20:36:03 +0200

    update new version 2.0.2 Build 2221

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class sublime_text_2 {
   package { 'SublimeText2':
     provider => 'appdmg',
-    source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg';
+    source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg';
   }
 
   file { "${boxen::config::bindir}/subl":
--- spec/classes/sublime_text_2_spec.rb
+++ spec/classes/sublime_text_2_spec.rb
@@ -4,7 +4,7 @@ describe 'sublime_text_2' do
   it do
     should contain_package('SublimeText2').with({
       :provider => 'appdmg',
-      :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg'
+      :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
     })
   end
 end
