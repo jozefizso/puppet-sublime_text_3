126d9f1084e2a092539d0bf7c10a96bc4fea66d6
Dan Dofter <dan@dofter.com>
2013-07-19 16:17:00 -0400

    Ensure that required directories are created

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,10 +9,7 @@ class sublime_text_2::config {
   $user_package_dir = "${package_dir}/User"
   $installed_package_dir = "${dir}/Installed Packages"
 
-  anchor { [
-    $dir,
-    $package_dir,
-    $user_package_dir,
-    $installed_package_dir
-  ]: }
+  file { [$dir, $package_dir, $user_package_dir, $installed_package_dir]:
+    ensure => directory
+  }
 }
