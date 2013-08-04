ea6285168e97d1f62fabc28fc9d7effc1c405769
Jozef Izso <jozef.izso@gmail.com>
2013-08-04 17:01:07 +0200

    Added test for subl command registration.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,8 @@
 #
 #     include sublime_text_2
 class sublime_text_2 {
+  include boxen::config
+
   package { 'SublimeText2':
     provider => 'appdmg',
     source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg';
--- spec/classes/sublime_text_2_spec.rb
+++ spec/classes/sublime_text_2_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
 describe 'sublime_text_2' do
+  let(:facts) { default_test_facts }
+
   it do
+    should include_class('boxen::config')
+
     should contain_package('SublimeText2').with({
       :provider => 'appdmg',
       :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
+    })
+
+    should contain_file("#{facts[:boxen_home]}/bin/subl").with({
+      :ensure  => 'link',
+      :target  => '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl',
+      :require => 'Package[SublimeText2]'
     })
   end
 end
