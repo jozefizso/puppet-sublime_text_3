54bf23de1e31953ae620af77e04f1b931fc010ac
Jozef Izso <jozef.izso@gmail.com>
2014-05-03 23:56:05 +0200

    Latest Sublime Text 3 dev build 3061

--- spec/classes/sublime_text_3_spec.rb
+++ spec/classes/sublime_text_3_spec.rb
@@ -13,7 +13,7 @@ describe 'sublime_text_3' do
 
     should contain_package('SublimeText3').with({
       :provider => 'appdmg',
-      :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg'
+      :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203061.dmg'
     })
 
     should contain_file(sublimedir).with_ensure('directory')
